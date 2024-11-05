9c957e7f3dfedca2ef86e87f1d64cdfc43dfee03
Pedro Neves <pedro.vasconcelosneves@gmail.com>
2024-11-04 22:03:40 -0300

    Atualização dos códigos

--- Dashboard.py
+++ Dashboard.py
@@ -1,56 +1,62 @@
-#Importando as bibliotecas nescessárias
-from Functions import Funcoes_Distribuicao
+# Importando as bibliotecas nescessárias
+from Functions import Funcoes
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sea
+import seaborn as sns
 import numpy as np
-import streamlit as st 
+import streamlit as st
+from PIL import Image, ImageDraw
+import base64
+from io import BytesIO
 
+# Carregando o banco de dados
 df = pd.read_csv('C:\\Prog\\Py\\ProjetosPessoais\\Pratica_e_pesquisa\\NESFIT\\data\\fitness_gym.csv')
 
-# Seletor de gráficos no painel lateral
-from PIL import Image, ImageDraw
 
-from PIL import Image, ImageDraw, ImageOps
+# Exibindo a imagem centralizada na barra lateral
+Funcoes.display_centered_image('src/imgs/nesfitv2.jpg')
+st.sidebar.image('src\\imgs\\satisfaction_stars.png')
+chart_type = st.sidebar.selectbox("Navegação", ["Tela de Início","Mercado","Avaliações e Cancelamentos", "Estudo das atividades","Horários","Informações do Projeto", "Informações do Grupo"])
 
-#imagem da side bar
-def make_circle_image(image_path, size=(100, 100)):
-    try:
-        # Open the image
-        image = Image.open(image_path)
-
-        # Resize the image
-        image = image.resize(size, Image.LANCZOS)
-
-        # Create a mask to make the image circular
-        mask = Image.new('L', size, 0)
-        draw = ImageDraw.Draw(mask)
-        draw.ellipse((0, 0, size[0], size[1]), fill=255)
-
-        # Create a circular cropped image
-        circular_image = ImageOps.fit(image, mask.size, Image.LANCZOS)
-        circular_image.putalpha(mask)
-
-        return circular_image
-    except Exception as e:
-        print(f"Error processing image: {e}")
-        return None  # Return None if there's an error
-
-st.sidebar.image(make_circle_image('src\\imgs\\nesfitv2.jpg.webp'))
-
-chart_type = st.sidebar.selectbox("Navegação", ["Tela de Início","Mercado", "Análises de Distribuição", "Informações do Grupo"])
 
 # Lógica para exibir os gráficos com base no seletor
-
+def get_base64_of_bin_file(bin_file):
+    with open(bin_file, 'rb') as f:
+        data = f.read()
+    return base64.b64encode(data).decode()
+# Página da Tela de Início
 if chart_type == "Tela de Início":
-    st.markdown("<h1 style='text-align: center;'>G2 NESFIT</h1>", unsafe_allow_html=True)
-    st.image('src\\imgs\\Tela Inicial.png')
+    img_path = "src/imgs/Tela_Inicial.jpg"  # Substitua pelo caminho da sua imagem
+    img_base64 = get_base64_of_bin_file(img_path)
 
+    # CSS para aplicar o background com a imagem em base64
+    background_style = f"""
+        <style>
+        .stApp {{
+            background-image: url("data:image/jpg;base64,{img_base64}");
+            background-size: cover;
+            background-position: center;
+            background-repeat: no-repeat;
+            background-attachment: fixed;
+        }}
+        </style>
+        """
+
+    # Injetando o CSS no Streamlit
+    st.markdown(background_style, unsafe_allow_html=True)
+
+
+
+# Página do Mercado
 elif chart_type == "Mercado":
+
+    # Duas colunas para o contexto de Mercado
     col1, col2 = st.columns(2)
     with col1:
+        # Adicona imagem a primeira coluna
         st.image('src\\imgs\\gym_mercado.png')
     with col2:
+        # Adiciona o texto de contexto de mercado à segunda coluna
         st.markdown("<h1 style='text-align: center;'>Contexto de mercado</h1>", unsafe_allow_html=True)
         st.markdown("""
                     ***********************
@@ -62,23 +68,122 @@ elif chart_type == "Mercado":
 
                     """) 
 
-    st.markdown("""************************""")     
+    st.markdown("""************************""")
+
+    # Duas colunas para as estatísticas de mercado    
     col1, col2 = st.columns(2)
     with col1:
+        # Adiciona o texto das estatísticas de mercado à primeira coluna
         st.markdown("<h1 style='text-align: center;'>Estatísticas do mercado</h1>", unsafe_allow_html=True)
         st.markdown("""
 
 *******************
-#### -O mercado fitness no Brasil fatura R$ 12 bilhões ao ano, de acordo com o Panorama Setorial de 2023   
+#### -O mercado fitness no Brasil fatura R$ 12 bilhões ao ano, de acordo com o Panorama Setorial de 2023.   
 
-#### -O valor transacionado com academias e produtos fitness cresceu cerca de 35% em 2023 comparado ao ano anterior, segundo estudo do Itaú Unibanco
+#### -O valor transacionado com academias e produtos fitness cresceu cerca de 35% em 2023 comparado ao ano anterior, segundo estudo do Itaú Unibanco.
 
 """)
         
          
     with col2: 
+        # Adiciona a imagem a segunda coluna
         st.image('src\\imgs\\gym_mercado2.png')
 
+#Avaliações e Cancelamentos        
+elif chart_type == "Avaliações e Cancelamentos":
+    st.markdown("<h1 style='text-align: center;'>Avaliações e Cancelamentos</h1>", unsafe_allow_html=True)
+    st.pyplot(Funcoes.cancelamento_plano(df))
+    st.markdown("""
+
+## Principais motivos de cancelamento:
+    
+#### Insatisfação: 40% 
+#### Custo alto: 25%
+#### Falta de tempo: 20%
+
+
+## Cancelamentos por plano:               
+### Basic: 80%
+#### Principais motivos:
+##### -Insatisfação
+##### -Custo alto
+                
+### Premium: 20%
+#### Principais motivos:
+##### -Falta de tempo
+##### -Custo alto
+
+
+*********************
+""")
+    
+
+    st.write("<h1 style='text-align: center;'>Satisfação média por plano</h1>", unsafe_allow_html=True)
+    
+    st.pyplot(Funcoes.satisfacao_plano(df))
+
+    st.markdown("""
+                
+### Observação:
+#### Classificação do plano básico está bem abaixo dos outros.
+
+
+************************
+""")
+
+    st.write("<h1 style='text-align: center;'>Problemas e soluções</h1>", unsafe_allow_html=True)
+    st.markdown("""
+    
+    ## Problemas:
+    ### - Altas taxas de cancelamento no plano Basic.
+    ### - Altos indices insatisfação no plano Basic.
+    ### - Muitas reclamações sobre preços altos nos planos Basic e Premium.
+
+    ## Em geral:
+    #### Está perdendo muitos cliente devido a baixa qualidade e alto valor do plano Basic.
+    
+    
+    ## Solução:
+    #### - Investigar como o plano Basic pode ser melhorado para atender melhor aos clientes.
+    #### - Repensar o preço do plano Basic para se encaixar melhor no orçamentos dos clientes.
+    """)
+
+
+
+#Horários
+
+elif chart_type == "Horários":
+    st.markdown("<h1 style='text-align: center;'>Horários</h1>", unsafe_allow_html=True)
+    st.write("## Frequencia diária dos alunos")
+    st.pyplot(Funcoes.frequencia_dia(df))
+    st.markdown("""
+
+                
+
+
+
+
+
+
+
+****************
+""")
+
+
+ #Informações do projeto
+elif chart_type == "Informações do Projeto":
+    st.markdown("<h1 style='text-align: center;'>Informações do projeto</h1>", unsafe_allow_html=True)
+    st.markdown("********************")
+    st.write('## Objetivo')
+    st.write('###### - Desenvolver uma plataforma digital que facilite a gestão e avaliação dos plano de academia do G2 NESFIT.')
+    st.write('## Metas')
+    st.write('###### - Aumentar a taxa de cancelamento de clientes.')
+    st.write('###### - Melhorar a qualidade do serviço.')
+    st.write('###### - Aumentar a satisfação dos clientes.')
+    st.write('## Estratégia')
+    st.write('###### - Utilizar dados de clientes e avaliações para melhorar o plano de')
+
+#Informações do grupo
 elif chart_type == "Informações do Grupo":
     st.markdown("<h1 style='text-align: center;'>Informações do projeto</h1>", unsafe_allow_html=True)
     st.markdown("********************")
@@ -86,55 +191,65 @@ elif chart_type == "Informações do Grupo":
     
     col1, col2,col3 = st.columns(3)
     with col1:
-        st.image(make_circle_image('src\\imgs\\Clarisse.jpg'))
-        st.write('###### Clarisse Lima')
+        st.image(Funcoes.make_circle_image('src\\imgs\\Clarisse.jpg'),width= 150)
+        st.write('##### Clarisse Lima')
         st.write('###### -Gravação do vídeo')
     with col2:
-        st.image(make_circle_image('src\\imgs\\Pedro.jpg'))
-        st.write('###### Pedro Neves')
+        st.image(Funcoes.make_circle_image('src\\imgs\\Pedro.jpg'),width= 150)
+        st.write('##### Pedro Neves')
         st.write('###### -Análise de Distribuição')
         st.write('###### -DashBoard')
     with col3:
-        st.image(make_circle_image('src\\imgs\\Malena.jpg'))
-        st.write('###### Malena Milani')
+        st.image(Funcoes.make_circle_image('src\\imgs\\Malena.jpg'),width= 150)
+        st.write('##### Malena Milani')
         st.write('###### -Repositório')
         st.write('###### -Organização')
 
     col1, col2,col3 = st.columns(3)
     with col1:
-        st.image(make_circle_image('src\\imgs\\Tiago.png'))
-        st.write('###### Tiago')
+        st.image(Funcoes.make_circle_image('src\\imgs\\Tiago.png'),width= 150)
+        st.write('##### Tiago Trindade')
+        st.write('###### -Criação do repositório')
+        st.write('###### -Análise Preditiva')
         st.write('###### -Análise de relacionamento')
         st.write('###### -Análise Temporal')
+        
     with col2:
-        st.image(make_circle_image('src\\imgs\\Gustavo.jpg'))
-        st.write('###### Gustavo')
-        st.write('###### -Edição de vídeo')
+        st.image(Funcoes.make_circle_image('src\\imgs\\Max.jpg'),width= 150)
+        st.write('##### Max Clay')
+        st.write('###### -Revisões')
     with col3:
-        st.image(make_circle_image('src\\imgs\\Malena.jpg'))
-        st.write('###### Malena Milani')
-        st.write('###### -Repositório')
-        st.write('###### -Organização')
+        st.image(Funcoes.make_circle_image('src\\imgs\\Malena.jpg'),width= 150)
+        st.write('##### Isabela')
+        st.write('###### -Análise Categórica')
+        st.write('###### -Estilização')
+    col1, col2,col3 = st.columns(3)
+    with col2:
+        st.image(Funcoes.make_circle_image('src\\imgs\\Gustavo.jpg'),width= 150)
+        st.write('##### Gustavo Nascimento')
+        st.write('###### -Edições')
     st.markdown("********************")
     st.markdown("""                
-### Descrição do papel do integrantes:
+### Descrição Detalhada:
+
+#### -Pedro Henrique Vasconcelos Neves (Mód. II):
+##### Tempo dedicado: 17hrs 12 min
+##### Tarefas:
+###### -Análise de distribuição (Arquivo Analise_Distribuicao.ipynb)
+###### -Criação e estilização do Dashboard (Arquivo Dashboard.py)
+###### -Criacão das funções (Arquivo Functions.py)
 
                 
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-
-
-
-
-
-
-
+                
     """)
 
-elif chart_type == "Análises de Distribuição":
-    st.write('# Analise de distribuição')
-    st.write("### Distribuição das atividades")
-    st.pyplot(Funcoes_Distribuicao.Distribuicao_Atividades(df))
+    
+#Análise de distribuição
+elif chart_type == "Estudo das atividades":
+    st.write('# Estudo das atividades da academia')
+    st.write("### Atividades mais realizadas")
+    st.pyplot(Funcoes.distribuicao_atividades(df))
     import streamlit as st
 
     st.markdown("""
@@ -142,40 +257,62 @@ elif chart_type == "Análises de Distribuição":
     #### 1. Zumba
     #### 2. Yoga
     #### 3. Musculação
-
-
-    #### Observações:
-    Aqui vai ser adicionada alguma descrição bonitinha ou algumas oportunidade de negocio descoberta a partir do grafico. Agora estou só enchendo linguiça pra ficar bonitinho.
+    *******************************    
     """)
 
     st.write("### Calorias X Atividades")
     st.write("#### Média de calorias queimadas por atividade")
-    st.pyplot(Funcoes_Distribuicao.Calorias_Atividade(df))
+    st.pyplot(Funcoes.calorias_atividade(df))
 
     st.markdown("""
     ### Top 3 atividades com mais calorias queimadas:
-    #### 1. Zumba
-    #### 2. Yoga
-    #### 3. Musculação
+    #### 1. CrossFit
+    #### 2. Spinning
+    #### 3. Boxing
+    
+    *******************************    
 
-
-    #### Observações:
-    Aqui vai ser adicionada alguma descrição bonitinha ou algumas oportunidade de negocio descoberta a partir do grafico. Agora estou só enchendo linguiça pra ficar bonitinho.
     """)
 
 
-    st.write("#### Distribuição das idades")
-    st.pyplot(Funcoes_Distribuicao.Distribuicao_Idades(df))
+    st.write("## Distribuição das Idades dos participantes")
+    st.pyplot(Funcoes.distribuicao_idades(df))
 
     st.markdown("""
     #### Idade Média do participantes: 42 Anos
     #### Idade Mínima encontrada: 18 anos
     #### Idade Máxima encontrada: 64 anos
 
-                    
-    #### Observações:
-    Em média a academia possui mais participantes em idades proximas a 40 anos que participam de atividades mas focadas em Mobilidade, força e vitalidade. Atividades como Jump, Boxe tem ficado para trás, isso pode ser resolvido tornando a academia mais atrante ao público jovem.
+    *******************************                
     """)
+
+    st.write('## Categoria das atividades')
+    st.pyplot(Funcoes.activity_category(df))
+
+    st.markdown("""
+    ### Principais categorias:
+    #### 1. Bem estar
+    #### 2. Dança
+    #### 3. Força
+    """)
+    
+    st.write("<h1 style='text-align: center;'>Problemas e soluções</h1>", unsafe_allow_html=True)
+    st.markdown("""
+    
+    ## Problemas:
+    ### - A atividade Jump tem poucos participantes.
+    ### - A ativide Jump queima poucas calorias.
+    ### - A atividade Jump não se enquadra com o público da academia.
+
+    ## Em geral:
+    #### A atividade Jump não está sendo lucrativa o suficiente e fora do padrão do público da academia.
+    
+    
+    ## Solução:
+    #### - Analisar como é possível substituir a atividade Jump por outra que se encaixe mais ao nosso público, como Zumba ou Yoga.
+    #### - Repensar o preço do plano Basic para se encaixar melhor no orçamentos dos clientes.
+    """)
+    
 
 
 
---
+++ Functions/Funcoes.py
@@ -0,0 +1,252 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import numpy as np
+import streamlit as st
+from PIL import Image, ImageDraw
+import base64
+from io import BytesIO
+
+def distribuicao_atividades(df):#Grafico de distribuição das ativades 
+
+#Seleciona a coluna activity do dataframe df, que contém informações sobre os tipos de atividades praticadas na academia.
+    activity = df.groupby('activity')['activity'].count().reset_index(name="Quantidade")
+    activity.sort_values(by='Quantidade', ascending=False, inplace=True)
+
+
+    #Configura o tamanho da figura e define uma cor para a área externa da figura.
+    plt.figure(figsize=(10,5), facecolor = '#0A0D0F')
+
+    #Criando um histograma com as atividades praticadas na academia.
+    plt.bar( x= activity['activity'], height = activity['Quantidade'],color = '#0A2538')
+
+    #Alterando a cor de fundo do gráfico.
+    plt.gca().set_facecolor('#E2CC9C')
+
+
+    #Adiciona e personaliza a grade do gráfico.
+    plt.grid(True,color = '#0A2538', linestyle = "--", alpha = 0.7)
+
+    #Adicionando um título ao gráfico, define a cor do título e o tamanho do título.
+    plt.title('Distribuição das atividades na academia', fontsize=16, color='#E64B2D')
+
+    #Labels e ticks.
+    plt.xlabel('Activity', fontsize=14, color='#E64B2D')
+    plt.ylabel('Frequency', fontsize=14, color='#E64B2D')
+    plt.xticks(color ='#E2CC9C',fontsize=10)
+    plt.yticks(color ='#E2CC9C',fontsize=10)
+
+    #Apresenta o gráfico.
+    return plt
+
+def distribuicao_idades(df):
+    # Pegando as idades
+    ages = df['age']
+
+    # Configura o tamanho da figura e define uma cor para a área externa da figura.
+    plt.figure(figsize=(10, 5), facecolor='#0A0D0F')
+
+    # Criando um histograma com as idades
+    plt.hist(ages, bins=20, color='#E64B2D', edgecolor='black', alpha=0.7, density= True)
+
+    # Alterando a cor de fundo do gráfico.
+    plt.gca().set_facecolor('#E2CC9C')
+
+    # Adicionando e estilizando a grade
+    plt.grid(True, color='#0A2538', alpha=0.7, linestyle='--')
+
+    # Adicionando um título ao gráfico, define a cor do título e o tamanho do título.
+    plt.title('Distribuição das idades na academia', color='#E2CC9C', fontsize=16)
+
+    # Labels e ticks
+    plt.xlabel('Age', color='#E2CC9C', fontsize=14)
+    plt.ylabel('Count', color='#E2CC9C', fontsize=14)
+    plt.xticks(color='#E2CC9C', fontsize=10)
+    plt.yticks(color='#E2CC9C', fontsize=10)
+
+    # Criando uma linha de densidade (KDE)
+    sns.kdeplot(ages, color='#0A2538', linewidth=2, label='Densidade')
+
+    plt.legend()
+
+    return plt
+
+
+
+def calorias_atividade(df):
+    #Agrupa a coluna activity com uma coluna formada pela média de calorias queimadas em cada atividade
+    calorias_atividade = df.groupby('activity')['calories_burned'].mean().reset_index()
+
+    #Ordena a coluna pela média de calorias queimadas em ordem decrescente
+    calorias_atividade.sort_values(by='calories_burned', ascending=False, inplace=True)
+
+    #Configura o tamanho da figura e define uma cor para a área externa da figura.
+    plt.figure(figsize=(10,5), facecolor = '#0A0D0F')
+    #Cria um gráfico de barras com a média de calorias queimadas por atividade na academia.
+    plt.bar(x=  calorias_atividade['activity'], height = calorias_atividade['calories_burned'],color = '#0A2538')
+
+    #Alterando a cor de fundo do gráfico.
+    plt.gca().set_facecolor('#E2CC9C')
+
+    #Adicionando e estilizando a grade
+    plt.grid(True,color = '#0A2538', linestyle = "--", alpha = 0.7)
+
+    #Adicionando um título ao gráfico, define a cor do título e o tamanho do título.
+    plt.title('Media de calorias queimadas por atividade', fontsize=16, color='#E64B2D')
+
+    #Labels e ticks.
+    plt.xlabel('Atividades', fontsize=14, color='#E64B2D')
+    plt.ylabel('Média de calorias queimadas', fontsize=14, color='#E64B2D')
+    plt.xticks(color ='#E2CC9C',fontsize=10)
+    plt.yticks(color ='#E2CC9C',fontsize=10)
+
+    #Mostra o gráfico.
+    return plt
+
+
+
+def satisfacao_plano(df):
+    # Calculando a média de satisfação para cada tipo de plano
+    media_satisfacao = df.groupby("membership_type")["satisfaction_score"].mean()
+
+    # Definido o tamanaho do gráfico
+    plt.figure(figsize=(10, 6), facecolor='#0A2538')
+
+    # plotando o gráfico
+    plt.plot(media_satisfacao.index, media_satisfacao.values, marker='o', color="#E84A2E", linewidth=2)
+
+    # Definindo a aparência do gráfico
+    plt.title("Média de Satisfação por Tipo de Plano", color="#E84A2E", fontsize=14)
+    plt.xlabel("Tipo de Plano", color="#E84A2E", fontsize=12)
+    plt.ylabel("Média de Satisfação (1 a 5)", color="#E84A2E", fontsize=12)
+    plt.xticks(color='#E84A3E')
+    plt.yticks(color='#E84A3E')
+    plt.gca().set_facecolor('#E2E4E7')
+    plt.grid(True)
+
+    # Apresentando o gráfico
+    return plt
+
+def cancelamento_plano(df):
+    # Tamanho e cor da figura
+    plt.figure(figsize=(12, 6), facecolor='#E84A2E')
+
+    # Paleta de cores das barras
+    colors = ['#A4B8C4', '#BF5B32', '#E84A2E', '#0A2538', '#E2CC9C']
+
+    # Plotando o gráfico com seaborn por permitir a melhor organização dos dados
+    sns.countplot(data=df, x="membership_type", hue="cancellation_reason", palette=colors)
+
+    # Customização
+    plt.title("Motivo de Cancelamento por Tipo de Plano", fontsize=16)
+    plt.xlabel("Tipo de Plano", fontsize=14)
+    plt.ylabel("Quantidade", fontsize=14)
+    plt.legend(title="Motivo de Cancelamento")
+    plt.grid(True)
+
+    return plt
+
+# Função que centraliza uma imagem usando HTML no Streamlit
+def centralize_image(image_path, width=None):
+    image_html = f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="{image_path}" style="{'width: ' + str(width) + 'px;' if width else ''}">
+        </div>
+    """
+    st.sidebar.markdown(image_html, unsafe_allow_html=True)
+
+#Função para arrendondar imagem
+def make_circle_image(image_path):
+    img = Image.open(image_path).convert("RGBA")
+    bigsize = (img.size[0] * 3, img.size[1] * 3)
+    mask = Image.new("L", bigsize, 0)
+    draw = ImageDraw.Draw(mask)
+    draw.ellipse((0, 0) + bigsize, fill=255)
+    mask = mask.resize(img.size, Image.LANCZOS)
+    img.putalpha(mask)
+    return img
+
+# Função para converter imagem em base64 para HTML
+def image_to_base64(img):
+    buffered = BytesIO()
+    img.save(buffered, format="PNG")
+    return base64.b64encode(buffered.getvalue()).decode()
+
+# Exibir a imagem centralizada
+def display_centered_image(image_path):
+    img = make_circle_image(image_path)
+    img_base64 = image_to_base64(img)
+    img_html = f"""
+        <div style="display: flex; justify-content: center;">
+            <img src="data:image/png;base64,{img_base64}" style="border-radius: 50%; width: 150px;">
+        </div>
+    """
+    st.sidebar.markdown(img_html, unsafe_allow_html=True)
+
+
+def plano_categoria(df):
+    colors = ['#0A2538', '#BF5B32', '#E84A2E' , '#E2CC9C']
+    df_activity_category_membership_type = df.groupby(['activity_category','membership_type'])['membership_type'].count().reset_index(name='membership_count')
+    plt.figure(figsize=(12, 6), facecolor= '#0A0D0F')
+    sns.barplot(data=df_activity_category_membership_type, x='activity_category', y='membership_count', hue='membership_type', palette=colors)
+
+    plt.grid(True, color ='#0A0D0F', linestyle='--')
+
+    plt.gca().set_facecolor('#E2CC9C')
+
+    # Add title and labels
+    plt.title('Membership Count by Activity Category and Membership Type',color= '#E84A2E',fontsize = 16)
+    plt.xlabel('Activity Category',color= '#E84A2E', fontsize = 14)
+    plt.ylabel('Membership Count',color= '#E84A2E', fontsize =14)
+
+    # Display the legend
+    plt.legend(title='Membership Type')
+    return plt
+
+
+def activity_category(df):
+    colors = ['#0A2538', '#BF5B32', '#E84A2E' , '#E2CC9C']
+    df_activity_category_membership_type = df.groupby(['activity_category','membership_type'])['membership_type'].count().reset_index(name='membership_count')
+    plt.figure(figsize=(12, 6), facecolor= '#0A0D0F')
+    sns.barplot(data=df_activity_category_membership_type, x='activity_category', y='membership_count', hue='membership_type', palette=colors)
+
+    plt.grid(True, color ='#0A0D0F', linestyle='--')
+
+    plt.gca().set_facecolor('#E2CC9C')
+
+    # Add title and labels
+    plt.title('Membership Count by Activity Category and Membership Type',color= '#E84A2E',fontsize = 16)
+    plt.xlabel('Activity Category',color= '#E84A2E', fontsize = 14)
+    plt.ylabel('Membership Count',color= '#E84A2E', fontsize =14)
+    plt.xticks(color ='#E2CC9C',fontsize=10)
+    plt.yticks(color ='#E2CC9C',fontsize=10)
+
+    # Display the legend
+    plt.legend(title='Membership Type')
+    return plt
+    
+
+
+def frequencia_dia(df):
+    # Converter a coluna de time para formato datetime
+    df["time"] = pd.to_datetime(
+    df["time"], format="%H:%M:%S"
+    ).dt.hour
+
+    # Agrupar por horário
+    hourly_pattern = df.groupby("time").size()
+
+    # Plotando gráfico de barras para o padrão por horário
+    plt.figure(figsize=(10, 6), facecolor='#0A2538')
+    plt.bar(
+    hourly_pattern.index, hourly_pattern.values, color='#BF5B32',
+    linewidth=0.6, edgecolor='black'
+    )
+    plt.gca().set_facecolor('#A4B8C4')
+    plt.title("Frequência por Horário do Dia", color='#E2CC9C', fontsize=16)
+    plt.xlabel("Hora", color='#E2CC9C', fontsize=14)
+    plt.ylabel("Número de Check-ins", color='#E2CC9C', fontsize=14)
+    plt.xticks(color='#E2CC9C', fontsize=10)
+    plt.yticks(color='#E2CC9C', fontsize=10)
+    plt.grid(True, color='gray')
+    return plt
